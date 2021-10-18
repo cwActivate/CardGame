2c5c71963e24baeafe6a5fcb4a98c54ed9a8a645
Chin Wee Kerk <kerkchinwee@hotmail.co.uk>
2021-10-18 09:07:51 +0800

    completed the game board

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import {
+  Modal,
   StyleSheet,
   Text,
+  TouchableNativeFeedback,
   View,
 } from 'react-native';
 
@@ -12,13 +14,65 @@ const styles = StyleSheet.create({
   column: {
     flexDirection: "column",
   },
-  fullStretched: {
+  container: {
     flex: 1,
+  },
+  fullStretched: {
     alignItems: "stretch",
     justifyContent: "space-between",
   },
+  header: {
+    alignItems: "center",
+    backgroundColor: "gray",
+    flexDirection: "row",
+    height: 50,
+    justifyContent: "space-between",
+  },
+  headerGroup: {
+    alignItems: "center",
+    flexDirection: "row",
+    height: 50,
+  },
+  modal: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalMessage: {
+    color: "black",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  modalTitle: {
+    color: "black",
+    fontSize: 24,
+    textAlign: "center",
+  },
+  modalCloseButton: {
+    borderRadius: 10,
+  },
+  modalCloseButtonText: {
+    color: "cyan",
+    fontSize: 20,
+    height: 50,
+    textAlign: "center",
+  },
   row: {
     flexDirection: "row",
+  },
+  restartButtonText: {
+    color: "lightblue",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  stepText: {
+    color: "lightblue",
+    fontSize: 24,
+    textAlign: "center",
+  },
+  stepTitle: {
+    color: "white",
+    fontSize: 24,
+    textAlign: "center",
   },
 });
 
@@ -31,42 +85,75 @@ const Board = () => {
     const cardValues = [0, 3, 6, 9].map((i) => values.slice(i, i+3));
     return cardValues;
   };
-  const [ cardValues ] = useState(getCardValues());
-  const [ isMatched, setIsMatched ] = useState(cardValues.map(() => [ false, false, false ]));
+  const [ cardValues, setCardValues ] = useState(getCardValues());
   const [ flippedCards, setFlippedCards ] = useState([]);
+  const getInitialMatchedState = () => cardValues.map(() => [ false, false, false ]);
+  const [ isMatched, setIsMatched ] = useState(getInitialMatchedState());
+  const [ matchedCount, setMatchedCount ] = useState(0);
   const [ steps, setSteps ] = useState(0);
+
   const onFlip = (row: number, column: number, value: number) => {
     setSteps(steps+1);
     if (flippedCards.length > 0) {
+      const [ r, c, v ] = flippedCards;
+      setFlippedCards([]);
       if (flippedCards[2] === value) {
         const isMatchedState = isMatched.map((state) => [ ...state ]);
         isMatchedState[row][column] = true;
-        isMatchedState[flippedCards[0]][flippedCards[1]] = true;
+        isMatchedState[r][c] = true;
         setIsMatched(isMatchedState);
+        setMatchedCount(matchedCount+1);
       }
-      setFlippedCards([]);
     } else {
       setFlippedCards([row, column, value]);
     }
   };
 
+  const reset = () => {
+    setCardValues(getCardValues());
+    setFlippedCards([]);
+    setIsMatched(getInitialMatchedState());
+    setMatchedCount(0);
+    setSteps(0);
+  };
+
   return (
     <>
-      <View>
-        <Text>{steps}</Text>
-      </View>
-      <View style={[ styles.column, styles.fullStretched ]}>
-      {[0, 1, 2, 3].map((r) => (
-        <View key={r} style={[ styles.fullStretched, styles.row ]}>
-          {[0, 1, 2].map((c) => <Card
-            column={c}
-            key={c}
-            isMatched={isMatched[r][c]}
-            onFlip={onFlip}
-            row={r}
-            value={cardValues[r][c]}
-          />)}
+      <Modal
+        animationType={"fade"}
+        visible={true}
+      >
+        <View style={[styles.container, styles.modal]}>
+          <Text style={styles.modalTitle}>Congratulations!</Text>
+          <Text style={styles.modalMessage}>You win the game by {steps} steps!</Text>
+          <TouchableNativeFeedback onPress={reset} style={styles.modalCloseButton}>
+            <Text style={styles.modalCloseButtonText}>Try another round</Text>
+          </TouchableNativeFeedback>
         </View>
+      </Modal>
+      <View >
+        <View style={[ styles.header ]}>
+          <TouchableNativeFeedback onPress={reset}>
+            <Text style={styles.restartButtonText}>RESTART</Text>
+          </TouchableNativeFeedback>
+          <View style={[ styles.headerGroup ]}>
+            <Text style={styles.stepTitle}>STEPS: </Text>
+            <Text style={styles.stepText}>{steps}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={[ styles.column, styles.container, styles.fullStretched ]}>
+        {[0, 1, 2, 3].map((r) => (
+          <View key={r} style={[ styles.container, styles.fullStretched, styles.row ]}>
+            {[0, 1, 2].map((c) => <Card
+              column={c}
+              key={c}
+              isMatched={isMatched[r][c]}
+              onFlip={onFlip}
+              row={r}
+              value={cardValues[r][c]}
+            />)}
+          </View>
         ))}
       </View>
     </>
